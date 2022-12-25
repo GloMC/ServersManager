e7911cc46f0595b0fe23281a341326dca24598a8
mohammed jasem alaajel <xrambad@gmail.com>
2022-12-25 16:26:31 +0400

    add run task for Velocity

--- LoadBalancer-API-Velocity/build.gradle.kts
+++ LoadBalancer-API-Velocity/build.gradle.kts
@@ -1,12 +1,18 @@
 plugins {
     id("java")
     id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("xyz.jpenilla.run-velocity") version "2.0.0"
 }
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://repo.papermc.io/repository/maven-public/")} // velocity repo
+    maven { url = uri("https://repo.papermc.io/repository/maven-public/") } // velocity repo
 }
+
+tasks.runVelocity {
+    velocityVersion("3.1.2-SNAPSHOT")
+}
+
 
 dependencies {
     implementation(project(":LoadBalancer-Common"))
